4b0c1e55609d3b82a19b254b2055c67c18a1c22e
kyledoesdev <kyledoesdev@gmail.com>
2025-09-06 14:28:06 -0400

    going to keep the scope of this package small

--- src/EssentialsServiceProvider.php
+++ src/EssentialsServiceProvider.php
@@ -3,9 +3,7 @@
 namespace Kyledoesdev\Essentials;
 
 use Kyledoesdev\Essentials\Commands\MakeActionCommand;
-use Kyledoesdev\Essentials\Commands\PublishStubsCommand;
 use Kyledoesdev\Essentials\Providers\MacroServiceProvider;
-use Kyledoesdev\Essentials\Services\PublishStubsService;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -20,21 +18,11 @@ class EssentialsServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('essentials')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigrations([
-                'create_users_table'
-            ])
             ->hasCommands([
                 MakeActionCommand::class,
-                PublishStubsCommand::class,
             ]);
     }
 }
--- src/Providers/MacroServiceProvider.php
+++ src/Providers/MacroServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Kyledoesdev\Essentials\Providers;
 
 use Carbon\Carbon;
-use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\ServiceProvider;
 
 class MacroServiceProvider extends ServiceProvider
@@ -11,8 +10,6 @@ class MacroServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::macro('inUserTimezone', function () {
-            /** @var Carbon $this */
-            /** @var ?Authenticatable $user */
             $user = auth()->user();
             
             return $this->tz($user?->timezone ?? timezone());
