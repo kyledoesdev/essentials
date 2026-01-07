81b2ec0f43aaa760f39211037b3dc0e6dd7d6b38
kyledoesdev <kyledoesdev@gmail.com>
2026-01-06 19:08:11 -0500

    fix namespace

---
+++ src/Middleware/IsDeveloper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kyledoesdev\Essentials\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsDeveloper
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && ! auth()->user()->is_dev) {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
---
+++ src/Stats/LoginStat.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Kyledoesdev\Essentials\Stats;
+
+use Spatie\Stats\BaseStats;
+
+class LoginStat extends BaseStats {}
